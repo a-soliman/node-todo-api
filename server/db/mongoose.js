12b79696ba6f8ff24843710a862156d4ad9862a8
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-01 11:48:52 -0800

    trying the dynamic dbs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-// const dbURL		= require('./../config/config');
+const dbURL		= require('./../config/config');
 
 mongoose.Promise = global.Promise
 
 
-var dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
+//var dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
 mongoose.connect(dbURL);
 
 
