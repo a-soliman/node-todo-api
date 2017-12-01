c191d8f046abdeb16b5009555f825b6333c8f5e5
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-01 11:51:54 -0800

    degugging db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
-const dbURL		= require('./../config/config');
+
 
 mongoose.Promise = global.Promise
+
+var env = process.env.NODE_ENV || 'development';
+var dbURL;
+
+if(env === 'development') {
+
+	 dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
+} else if (env === 'test') {
+	
+	 dbURL = 'mongodb://ahmed_soliman:abc123@ds125906.mlab.com:25906/node-todo-api-test'
+};
 
 
 //var dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
