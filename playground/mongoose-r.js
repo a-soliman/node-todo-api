461707d760f2ad1c444d6a325a1b28a71f3aafdb
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-01 08:40:29 -0800

    working with findByIdAndRemove() as well as findOneAndRemove()

--- playground/mongoose-r.js
+++ playground/mongoose-r.js
@@ -3,6 +3,17 @@ const {  mongoose } = require('./../server/db/mongoose');
 const { Todo } 		= require('./../server/models/todo');
 
 // Todo.remove => REMOVES ALL
-Todo.remove({}).then((result) => {
-	console.log(result);
-});
+
+// Todo.remove({}).then((result) => {
+// 	console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findOneAndRemove({text: "First test todo"}).then((doc) => {
+// 	console.log(doc)
+// });
+
+// Todo.findByIdAndRemove
+Todo.findByIdAndRemove('5a21854bab1ad97c6762b467').then((doc) => {
+	console.log(doc)
+})
