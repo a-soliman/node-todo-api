0c751a340d30fc0c5e6fd4dc8f5f42232e00413b
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-12 15:10:27 -0800

    updated the config files

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,14 @@
-// var env = process.env.NODE_ENV || 'development';
-// var dbURL;
+var env = process.env.NODE_ENV || 'development';
 
-// if(env === 'development') {
 
-// 	 dbURL = 'mongodb://ahmed_soliman:abc123@ds123556.mlab.com:23556/node-todo-api';
-// } else if (env === 'test') {
-	
-// 	 dbURL = 'mongodb://ahmed_soliman:abc123@ds125906.mlab.com:25906/node-todo-api-test'
-// };
+if( env === 'development' || env === 'test' ) {
+	var config = require('./config.json');
 
-// module.exports = dbURL;
+	var envConfig = config[env];
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
+};
+
+
