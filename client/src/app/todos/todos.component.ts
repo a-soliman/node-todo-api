b4332c252e743cf68777b9f20eb5a4a104178991
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-22 10:11:00 -0800

    coded the view and the component logic for the todos component get/ post req.

--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { TodosService } from '../todos.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
+  providers: [ TodosService ]
 })
+
 export class TodosComponent implements OnInit {
+	@Input() user;
+	todos;
 
-  constructor() { }
+	addTodoForm: FormGroup;
 
-  ngOnInit() {
-  }
+	constructor( private _todosService: TodosService,
+				 private fb: FormBuilder ) { 
+		this.addTodoForm = fb.group({
+			'todo_text': [null, Validators.compose([ Validators.required, Validators.minLength(6)])]
+		});
+	}
+
+	ngOnInit() {
+		this.getTodos()
+	}
+
+	getTodos() {
+		this._todosService.getTodos(this.user)
+			.subscribe((res) => {
+				this.todos = res.todos;			})
+	}
+
+	addTodo( todoFormValue ) {
+		let todoText = todoFormValue.todo_text;
+		
+		this._todosService.addTodo(todoText, this.user)
+			.subscribe((res) => {
+				console.log('Added')
+			})
+		this.getTodos();
+	}
+
+
 
 }
