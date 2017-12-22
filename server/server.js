799d86c11d41405a73ae5c0c223d9be619edef29
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-22 11:10:57 -0800

    modified the server's configuration to accept cross-origin PATCH requests

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
 	//res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader('Access-Control-Allow-Headers', '*');
