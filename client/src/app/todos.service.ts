c4d4091cec5e098b183e4e7bfdffd025b3980ef0
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-22 10:10:17 -0800

    coded the todos service runing an http get req. to get the todos list per user and a post request to addTodo

--- client/src/app/todos.service.ts
+++ client/src/app/todos.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class TodosService {
 
-  constructor() { }
+  constructor( private _http: Http ) { }
+
+  getTodos( userObject ) {
+
+	let url = 'http://localhost:3000/todos'
+
+	let headers = new Headers();
+	headers.append('Content-Type', 'application/json');
+	headers.append('x-auth', userObject['x-auth']);
+	
+	return this._http.get(url, {headers: headers} )
+		.map(res=> res.json())
+
+  }
+
+  addTodo( text, user ) {
+  	let url = 'http://localhost:3000/todos';
+  	text = {text}
+  	console.log(text)
+  	let headers = new Headers();
+  	headers.append('Content-Type', 'application/json');
+  	headers.append('x-auth', user['x-auth']);
+
+  	return this._http.post(url, JSON.stringify(text), {headers})
+  		.map(res => res.json())
+  }
+
+  // editTodo
+
+  // deleteTodo
 
 }
