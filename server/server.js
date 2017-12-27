bf9ab90fa7462392efa90f6d1bddcaebe1481847
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-27 15:45:47 -0800

    deugging

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ let app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 app.use(bodyParser.json());
 
